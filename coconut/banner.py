94bba58514555aa0ab03d5a7fcf21fa2f72d4d69
vimn4n0 <vim.n4n0@gmail.com>
2023-02-16 14:11:30 +0800

    change

--- coconut/banner.py
+++ coconut/banner.py
@@ -10,7 +10,7 @@ banner = """
 ░╚════╝░░╚════╝░░╚════╝░░╚════╝░╚═╝░░╚══╝░╚═════╝░░░░╚═╝░░░
                                                    v1.337
 
-{yellow}Greetz ~ {red}Vim Nano{green} | Daeng Cyber{yellow} | The Wich{blue} | MEH{cyan} | COCONUT CREW""".format(
+{yellow}Greetz ~ {red}Vim Nano{green} | Daeng Cyber{yellow} | The Wich{blue} | MEH{cyan} | COCONUT CREW{yellow} | ALGO TEAM""".format(
     
     green = fg + sb,
     blue = fb + sb,
